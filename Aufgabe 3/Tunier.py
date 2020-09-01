62a81b58cc25bf3814c817b2fb1024b73933dac3
Atlasfreak <60364953+Atlasfreak@users.noreply.github.com>
2020-09-01 21:53:41 +0200

    reworked winning condition and made function simulate fights

--- Aufgabe 3/Tunier.py
+++ Aufgabe 3/Tunier.py
@@ -1,22 +1,32 @@
 from random import *
 
-def createUrn(strengthP1, strengthP2):
-    # Urne mit Kugeln basierend auf Spieler Stärke generieren
-    urn = []
-    
-    while (strengthP1 + strengthP2) > 0:
-        if randint(0,2) and strengthP1 > 0:
-            # Kugel für Spieler 1 hinzufügen
-            urn.append(0)
-            strengthP1 -= 1
-        elif strengthP2 > 0:
-            # Kugel für Spieler 2 hinzufügen
-            urn.append(1)
-            strengthP2 -= 1
+def fight(strengthP1, strengthP2):
+    # strengthP1 = int
+    # strengthP2 = int
+    #
+    # Simuliere einen Kampf zwischen 2 Spielern.
 
-    return urn
+    rand = randint(1, strengthP1 + strengthP2)
+    if rand <= strengthP2:
+        #print("Spieler 2 hat gewonnen!")
+        return True
+    elif rand - strengthP2 <= strengthP1:
+        #print("Spieler 1 hat gewonnen!")
+        return False
 
-def chooseBall(urn):
-    rand = random(len(urn))
-    return urn[rand]
+def simulateFights(strengthP1, strengthP2, times):
+    # strengthP1 = int
+    # strengthP2 = int
+    # times = int
+    # 
+    # Simuliere times Kämpfe zwischen 2 Spielern.
 
+
+    winsP1 = 0
+    winsP2 = 0
+    for i in range(times):
+        if fight(strengthP1, strengthP2):
+            winsP2 += 1
+        else:
+            winsP1 += 1
+    return (winsP1/winsP2, winsP1, winsP2)
