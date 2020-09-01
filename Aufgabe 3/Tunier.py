63bd3dd8c86cfde60c6afcc1dd1a43bc682fa04d
Atlasfreak <60364953+Atlasfreak@users.noreply.github.com>
2020-09-01 20:58:43 +0200

    replaced placeholder function with createUrn

--- Aufgabe 3/Tunier.py
+++ Aufgabe 3/Tunier.py
@@ -1,4 +1,17 @@
 from random import *
 
 def createUrn(strengthP1, strengthP2):
-    pass
+    # Urne mit Kugeln basierend auf Spieler Stärke generieren
+    urn = []
+    while (strengthP1 + strengthP2) > 0:
+        if randint(0,2) and strengthP1 > 0:
+            # Kugel für Spieler 1 hinzufügen
+            urn.append(0)
+            strengthP1 -= 1
+        elif strengthP2 > 0:
+            # Kugel für Spieler 2 hinzufügen
+            urn.append(1)
+            strengthP2 -= 1
+
+    return urn
+
